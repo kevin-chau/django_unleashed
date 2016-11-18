5580083619cb10dda48af55d0bbc6c4c91ceb897
kevinchau321 <kevinchau321@gmail.com>
2016-11-17 17:22:58 -0800

    deleted hello world app

--- suorganizer/suorganizer/urls.py
+++ suorganizer/suorganizer/urls.py
@@ -16,9 +16,6 @@ Including another URLconf
 from django.conf.urls import url, include
 from django.contrib import admin
 
-from helloworld.views import greeting
-
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
-    url(r'^$', greeting),
 ]
