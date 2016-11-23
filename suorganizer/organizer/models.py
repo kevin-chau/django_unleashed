98fcbbda6b9cf7c4f3170bf6469541563f657a51
kevinchau321 <kevinchau321@gmail.com>
2016-11-22 21:22:16 -0800

    working on organizer models

--- suorganizer/organizer/models.py
+++ suorganizer/organizer/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
 # Create your models here.
+class Tag(models.Model):
+    name = models.CharField(max_length=31)
+    slug = models.SlugField()
+
+class Startup(models.Model):
+    name = models.CharField(max_length=31)
+    slug = models.SlugField()
+    descrription = models.TextField()
+    founded_date = models.DateField()
+    contact = models.EmailField()
+    website = models.URLField()
+
+class NewsLink(models.Model):
+    pass
